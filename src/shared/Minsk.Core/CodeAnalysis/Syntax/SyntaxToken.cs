7f5b96dd2bbeca0ee9cfd29f57877e6aeb0f37cc
Fr3gU <fr3gu77@gmail.com>
2020-03-25 19:20:29 +0100

    Derp

--- src/shared/Minsk.Core/CodeAnalysis/Syntax/SyntaxToken.cs
+++ src/shared/Minsk.Core/CodeAnalysis/Syntax/SyntaxToken.cs
@@ -17,6 +17,6 @@
 
         public object Value { get; }
         public override SyntaxKind Kind { get; }
-        public TextSpan Span => new TextSpan(Position, Text.Length);
+        public override TextSpan Span => new TextSpan(Position, Text.Length);
     }
 }
