89aba2f7ab0a7087d31db1f732307fe14c90f12a
Fr3gU <fr3gu77@gmail.com>
2020-03-25 19:16:17 +0100

    Added PrettyPrint of tree to SyntaxNode

--- src/app/Minsk.ConsoleApp/Program.cs
+++ src/app/Minsk.ConsoleApp/Program.cs
@@ -50,7 +50,7 @@ namespace Minsk.ConsoleApp
                 var color = Console.ForegroundColor;
                 Console.ForegroundColor = ConsoleColor.DarkGray;
 
-                if(showTree) PrettyPrint(syntaxTree.Root);
+                if(showTree) syntaxTree.Root.WriteTo(Console.Out);
 
                 Console.ForegroundColor = color;
 
@@ -84,32 +84,6 @@ namespace Minsk.ConsoleApp
 
                     Console.WriteLine();
                 }
-            }
-        }
-
-        private static void PrettyPrint(SyntaxNode node, string indent = "", bool isLast = true)
-        {
-            var marker = isLast ? "└── " : "├── ";
-
-            Console.Write(indent);
-            Console.Write(marker);
-            Console.Write(node.Kind);
-
-            if (node is SyntaxToken t && t.Value != null)
-            {
-                Console.Write(" ");
-                Console.Write(t.Value);
-            }
-
-            Console.WriteLine();
-
-            indent += isLast ? "    " : "│   ";
-
-            var lastChild = node.GetChildren().LastOrDefault();
-
-            foreach (var child in node.GetChildren())
-            {
-                PrettyPrint(child, indent, child == lastChild);
             }
         }
     }
