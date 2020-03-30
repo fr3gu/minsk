973aae3c5bb091a9c5ed9d76af112ef0b0776dc1
Fr3gU <fr3gu77@gmail.com>
2020-03-30 17:25:26 +0200

    Added tests for SourceText

---
+++ src/test/Minsk.Test/CodeAnalysis/Text/SourceTextTests/GetLineIndex_Should.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Minsk.Core.CodeAnalysis.Text;
+using NUnit.Framework;
+
+namespace Minsk.Test.CodeAnalysis.Text.SourceTextTests
+{
+    public class GetLineIndex_Should
+    {
+        [TestCase("aa\r\nbb\r\ncc\r\ndd\r\nee", 8, 2)]
+        [TestCase("aa\r\nbb\r\ncc\r\ndd\r\nee", 6, 1)]
+        [TestCase("en katt skickade ett paket\r\ntill en morbror som bodde i\r\nUkraina", 57, 2)]
+        [TestCase("en katt skickade ett paket\r\ntill en morbror som bodde i\r\nUkraina", 55, 1)]
+        public void IncludeLastLine(string text, int position, int expectedLineIndex)
+        {
+            var sut = SourceText.From(text);
+
+            var lineIndex = sut.GetLineIndex(position);
+
+            Console.WriteLine(sut.ToString());
+
+            Assert.That(lineIndex, Is.EqualTo(expectedLineIndex));
+        }
+    }
+}
