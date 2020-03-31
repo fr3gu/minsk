886cc9d365c0850d60cdcff5687d63f175197e4c
Fr3gU <fr3gu77@gmail.com>
2020-03-31 23:06:55 +0200

    Improved the REPL
    It now has multi-line editing, syntax coloring and history (PGUP/PGDN)

---
+++ src/app/Minsk.ConsoleApp/MinskRepl.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Minsk.Core.CodeAnalysis;
+using Minsk.Core.CodeAnalysis.Syntax;
+using Minsk.Core.CodeAnalysis.Text;
+
+namespace Minsk.ConsoleApp
+{
+    internal sealed class MinskRepl : Repl
+    {
+        private Compilation _previous;
+        private bool _showTree;
+        private bool _showProgram;
+        private readonly Dictionary<VariableSymbol, object> _variables;
+
+        public MinskRepl()
+        {
+            _showTree = false;
+            _showProgram = false;
+            _variables = new Dictionary<VariableSymbol, object>();
+        }
+
+        protected override void RenderLine(string line)
+        {
+            var tokens = SyntaxTree.ParseTokens(line);
+            foreach (var token in tokens)
+            {
+                var isKeyword = token.Kind.ToString().EndsWith("Keyword");
+                var isNumber = token.Kind == SyntaxKind.NumberToken;
+                if (isKeyword)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                }
+                else if (!isNumber)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                }
+                else
+                {
+
+                }
+
+                Console.Write(token.Text);
+
+                Console.ResetColor();
+            }
+        }
+
+        protected override void EvaluateMetaCommands(string input)
+        {
+            switch (input)
+            {
+                case "#showTree":
+                    _showTree = !_showTree;
+                    Console.WriteLine(_showTree ? "Showing parse tree." : "Not showing parse tree.");
+                    break;
+                case "#showProgram":
+                    _showProgram = !_showProgram;
+                    Console.WriteLine(_showProgram ? "Showing program." : "Not showing program.");
+                    break;
+                case "#cls":
+                    Console.Clear();
+                    break;
+                case "#reset":
+                    _previous = null;
+                    break;
+                default:
+                    base.EvaluateMetaCommands(input);
+                    break;
+            }
+        }
+
+        protected override void EvaluateSubmission(string text)
+        {
+            var syntaxTree = SyntaxTree.Parse(text);
+
+            var compilation = _previous == null ? new Compilation(syntaxTree) : _previous.ContinueWith(syntaxTree);
+
+            var color = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+
+            if (_showTree)
+                syntaxTree.Root.WriteTo(Console.Out);
+
+            if (_showProgram)
+                compilation.EmitTree(Console.Out);
+
+            var result = compilation.Evaluate(_variables);
+
+            var diagnostics = result.Diagnostics.ToArray();
+
+            Console.ForegroundColor = color;
+
+            if (!diagnostics.Any())
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(result.Value);
+                Console.ResetColor();
+                _previous = compilation;
+            }
+            else
+            {
+                foreach (var diagnostic in diagnostics)
+                {
+                    var lineIndex = syntaxTree.Text.GetLineIndex(diagnostic.Span.Start);
+                    var line = syntaxTree.Text.Lines[lineIndex];
+                    var lineNumber = lineIndex + 1;
+                    var character = diagnostic.Span.Start - line.Start + 1;
+
+                    Console.WriteLine();
+
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write($"({lineNumber}, {character}): ");
+                    Console.WriteLine(diagnostic);
+                    Console.ResetColor();
+
+                    var prefixSpan = TextSpan.FromBounds(line.Start, diagnostic.Span.Start);
+                    var suffixSpan = TextSpan.FromBounds(diagnostic.Span.End, line.End);
+
+                    var prefix = syntaxTree.Text.ToString(prefixSpan);
+                    var error = syntaxTree.Text.ToString(diagnostic.Span);
+                    var suffix = syntaxTree.Text.ToString(suffixSpan);
+
+                    Console.Write("    ");
+                    Console.Write(prefix);
+
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write(error);
+                    Console.ResetColor();
+
+                    Console.Write(suffix);
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        protected override bool IsCompleteSubmission(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return true;
+            }
+
+            var syntaxTree = SyntaxTree.Parse(text);
+
+            if (syntaxTree.Diagnostics.Any())
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
